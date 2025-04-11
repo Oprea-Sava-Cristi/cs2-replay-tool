96b4938e27f6c6152c7c2cecaabd0118f8f84d8b
Tudor230 <133231902+Tudor230@users.noreply.github.com>
2025-04-11 12:18:40 +0300

    Remove SFML Main and json import

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
 #include "renderer/Render.hpp"
 #include "analysis/Round.hpp"
 #include <iostream>
-#include <nlohmann/json.hpp>
-
-using json = nlohmann::json;
 
 int main() {
     Round round = createRound("");
